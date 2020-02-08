8f1b70f2dca64d86fdb028c2fd596a095238b004
RyoKoibuchi <ryouhb0504@icloud.com>
2020-02-08 16:46:38 +0900

    chapter3-4まで終了

--- amazon/views.py
+++ amazon/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import *
 
 # Create your views here.
 
 class Lp(generic.TemplateView):
     template_name = 'amazon/lp.html'
+
+
+class ItemList(generic.ListView):
+    model = Product
+    template_name = 'amazon/item_list.html'
+
+    def get_queryset(self):
+        products = Product.objects.all()
+        if 'q' in self.request.GET and self.request.GET['q'] != None:
+            q = self.request.GET['q']
+            products = products.filter(name__icontains = q)
+        return products
